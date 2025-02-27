b9f4cb6e1c48c9c12c5d7935688c4bec62ff822f
epbrijesh <epbrijesh@gmail.com>
2025-02-28 02:36:43 +0530

    Delete app/pages directory

--- app/pages/detail/detail-page.ts
+++
@@ -1,8 +0,0 @@
-import { NavigatedData, Page } from '@nativescript/core';
-import { DetailViewModel } from './detail-view-model';
-
-export function onNavigatingTo(args: NavigatedData) {
-  const page = <Page>args.object;
-  const odonate = args.context;
-  page.bindingContext = new DetailViewModel(odonate);
-}
