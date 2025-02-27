55687d4491ec2649c0c1d8ff0f4cc0f229f4b0b0
epbrijesh <epbrijesh@gmail.com>
2025-02-28 02:41:49 +0530

    Add files via upload

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.app',
+  id: 'com.thinkdigit.odonatesofkerala',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
