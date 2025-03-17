7452c6e9839667d81ef1ffedf5defde395e0fca0
Brijesh Pookkottur <epbrijesh@gmail.com>
2025-03-17 21:36:30 +0530

    Delete app/pages directory

--- app/pages/learn/learn-page.ts
+++
@@ -1,7 +0,0 @@
-import { NavigatedData, Page } from '@nativescript/core';
-import { LearnViewModel } from './learn-view-model';
-
-export function onNavigatingTo(args: NavigatedData) {
-  const page = <Page>args.object;
-  page.bindingContext = new LearnViewModel();
-}
